f1d0063ea36a8c5116dbb3513e05fb22fcc489f7
rachelriv <rachelriv94@yahoo.com>
2015-01-25 14:51:39 -0800

    add Makefile

--- loader.s
+++ loader.s
@@ -5,6 +5,7 @@ MEMINFO equ 1 << 1
 FLAGS equ FL_ALIGN | MEMINFO
 MAGIC equ 0x1BADB002    
 CHECKSUM equ -(MAGIC + FLAGS)
+KERNEL_STACK_SIZE equ 4096
 
 section .text:
 align 4
@@ -16,3 +17,9 @@ loader:
   mov eax, 0xCAFEBABE
 .loop:
   jmp .loop
+
+section .bss
+align 4
+kernel_stack:
+  resb KERNEL_STACK_SIZE
+  mov esp, kernel_stack + KERNEL_STACK_SIZE
