357697a011ae6caf5838a1759582ebec73e87aa6
rachelriv <rachelriv94@yahoo.com>
2015-03-04 15:02:17 -0800

    modify memmove

--- string.c
+++ string.c
@@ -13,13 +13,21 @@ size_t strlen(const char *buf) {
 
 
 // http://man7.org/linux/man-pages/man3/memmove.3.html
-// simple implementation of memmove
 void *memmove(void *dst, const void *src, size_t len) {
-  char *dstmem = (char*)dst;
-  char *srcmem = (char*)src;
-  size_t i;
-  for (i=0; i<len; i++) {
-    dstmem[i] = srcmem[i];
-  }
-  return dstmem;
+    char *dp = (char*)dst;
+    char *sp = (char*)src;
+
+    if (dp < sp) {
+    // overlap guarantee
+        while(len-- > 0){
+            *dp++ = *sp++;
+        }
+    } else {
+        dp += len;
+        sp += len;
+        while (len-- > 0){
+            *--dp = *--sp;
+        }
+    }
+    return dst;
 }
