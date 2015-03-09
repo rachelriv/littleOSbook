6f68426c6afe25b21ba3f628ab6dbbbcaf6d8890
Juan S Carrillo <juanscarrillo23@gmail.com>
2015-03-08 17:22:29 -0700

    Fixed extra spaces.

--- keyboard.c
+++ keyboard.c
@@ -7,8 +7,8 @@
 
 #define KBD_DATA_PORT 0x60
 static void keyboard_cb(registers_t regs);
-int scancodes[] = { 
-  0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
+int scancodes[]  = {
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
   '9', '0', '-', '=', '\b',	/* Backspace */
   '\t',			/* Tab */
   'q', 'w', 'e', 'r',	/* 19 */
@@ -18,13 +18,38 @@ int scancodes[] = {
  '\'', '`',   0,		/* Left shift */
  '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
   'm', ',', '.', '/',   0,				/* Right shift */
-  '*'
- };
+  '*',
+    0,	/* Alt */
+  ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+  '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+  '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
+};
 
 static void keyboard_cb(registers_t regs) {
   unsigned char scan_code = inb(KBD_DATA_PORT);
-  int c = scancodes[scan_code];   
-  printf("%c", c);
+  int c = scancodes[scan_code];
+  if(!(scan_code & 0x80)) printf("%c", c);
 }
 
 void init_keyboard() {
