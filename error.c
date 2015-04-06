14cb1caa29ae7938cb8e3c25a87809596f977548
rachelriv <rachelriv94@yahoo.com>
2015-04-06 13:38:24 -0700

    clean up

--- error.c
+++ error.c
@@ -4,13 +4,7 @@ extern void error(const char *message, const char *file, uint32_t line){
     // We encountered a massive problem and have to stop.
     asm("cli"); // Disable interrupts.
 
-    printf("ERROR(");
-    printf(message);
-    printf(") at ");
-    printf(file);
-    printf(":");
-    printf("%d", line);
-    printf("\n");
+    printf("ERROR( %s ) at %s: %d\n", message, file, line);
     // Halt by going into an infinite loop.
     while(1);
 }
